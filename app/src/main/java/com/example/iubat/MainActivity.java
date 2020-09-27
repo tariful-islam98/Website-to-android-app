d05eb4ddce500f0c2191ed883c00c40254d7e29b
tariful-islam98 <abir.tariful.islam@gmail.com>
2020-09-27 21:34:42 +0600

    First commit for the project

---
+++ app/src/main/java/com/example/iubat/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.iubat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.wbView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://iubat.edu/");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else super.onBackPressed();
+
+    }
+}
